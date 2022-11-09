f37f3c253678ca40904dc7e1949f491a67039f45
Orr <95436834+OscarRicoRo@users.noreply.github.com>
2022-11-09 21:14:02 +0000

    Update Main.java

--- spotify/src/main/java/es/ulpgc/spotify/downloader/Main.java
+++ spotify/src/main/java/es/ulpgc/spotify/downloader/Main.java
@@ -4,9 +4,6 @@ import es.ulpgc.spotify.downloader.controller.Controller;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        long Start = System.currentTimeMillis();
         new Controller().control();
-        long End = System.currentTimeMillis();
-        System.out.println(End-Start);
     }
 }
